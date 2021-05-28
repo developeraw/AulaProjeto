09662fba7cf99e0d9531df8cadc8f71bdec1bfea
alexandresturmerwolf <senhagit>
2021-05-28 19:56:02 -0300

    Começando a parte gráfica acrescentarndo uma classe inicial

---
+++ src/aulaprojeto/ClasseInicial.java
@@ -0,0 +1,20 @@
+/*
+ * Autor______: Alexandre Stürmer Wolf
+ * Utilidade__: Chamar a tela principal
+ * Data_______: 28/05/2021
+ * Dicas______: So chamar
+ */
+package aulaprojeto;
+
+/**
+ *
+ * @author wolfi
+ */
+public class ClasseInicial {
+    
+    public static void main(String[] args) {
+        MinhaTela minha = new MinhaTela();
+        minha.setVisible(true);
+    }
+    
+}
